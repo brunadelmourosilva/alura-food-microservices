d2a52279e17b9c2d60cb8a68827d32aab0a96ae3
Bruna Delmouro <brunadelmouro@gmail.com>
2022-09-05 16:21:58 -0300

    Initial project

---
+++ src/main/java/br/com/brunadelmouro/pagamentos/MicroserviceParaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunadelmouro.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceParaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceParaPagamentosApplication.class, args);
+	}
+
+}
